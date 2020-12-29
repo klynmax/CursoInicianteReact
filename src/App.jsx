3d2f34d7f3ef455d1b5d3473527ef400b345ba4d
Klynsman Guedes <klynsman.lemos@gmail.com>
2020-12-29 18:19:08 -0300

    Aula 33 - Componente controlado

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ import TabelaProdutos from './components/repeticao/TabelaProdutos';
 import ParOuImpar from './components/condicional/ParOuImpar';
 import UsuarioInfo from './components/condicional/UsuarioInfo';
 import DiretaPai from './components/comunicacao/DiretaPai';
-import IndiretaPai from './components/comunicacao/IndiretaPai.jsx'
+import IndiretaPai from './components/comunicacao/IndiretaPai'
+import Input from './components/formulario/Input'
 
 export default () => (
 
@@ -22,6 +23,10 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo="#11 - Componente Controlado (Input)" color="#E45F56">
+                <Input></Input>
+            </Card>
+
             <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
                 <IndiretaPai></IndiretaPai>
             </Card>
